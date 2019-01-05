85a00b290d4af0bbf57290da5caf9e3102a804c0
AnastasiaEs <anastasia.lukina.es@gmail.com>
2019-01-05 19:49:10 +0100

    Eliminar carpetas duplicadas

--- Python/XMLSAX/Myparser.py
+++
@@ -1,9 +0,0 @@
-import sys
-import xml.sax
-sys.path.append ('.\Mysax.py')
-from Mysax import ManejadorCatalago
-parser=xml.sax.make_parser()
-parser.setFeature(xml.sax.handler.feature_namespaces,0)            
-Handler=ManejadorCatalago()
-parser.setContentHandler(Handler)
-parser.parse("catalogos.xml")
